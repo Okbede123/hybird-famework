2f74f0dd1031f12e5642950ae1b29403f1463dcf
Admin <vuminhduc24081997@gmail.com>
2022-12-16 19:31:33 +0700

    update 16/12/2022 l;an 2

---
+++ cores/reportConfig/ExtentListener.java
@@ -0,0 +1,4 @@
+package src2.cores.reportConfig;
+
+public class ExtentListener {
+}
---
+++ cores/retryConfig/RetryAnalyzer.java
@@ -0,0 +1,5 @@
+package src2.cores.retryConfig;
+
+// dùng để chạy lại testcase
+public class RetryAnalyzer {
+}
