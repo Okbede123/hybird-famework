8cfe49a280550effdbac0f516243676c14b738f3
Admin <vuminhduc24081997@gmail.com>
2022-12-25 10:00:37 +0700

    update 25/12/2022 lan 4 ducautotest

--- src/Automation_test/BaiTapFluentWait_5.java
+++ src/Automation_test/BaiTapFluentWait_5.java
@@ -14,6 +14,7 @@ import java.util.function.Function;
 public class BaiTapFluentWait_5 {
     WebDriver driver;
 
+
     //khoi tao fluent wait dau vao WebElement
     FluentWait<WebElement> fluentWait;
 
